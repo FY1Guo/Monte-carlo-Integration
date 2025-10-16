f704fafa071d02d8f66d2e562a612fb9462d01ea
FY1Guo <fangyi.guo2@gmail.com>
2025-10-16 13:33:49 -0400

    add area

--- ellipse.py
+++ ellipse.py
@@ -1,8 +1,22 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+
+rng = np.random.default_rng()
+
 a = 5
 b = 2
+
+def area(N, rng=rng):
+    Abox = 4*a*b
+    x = rng.random(-a, a, size=N)
+    y = rng.random(-b, b, size=N)
+    inside = (x/a)**2 + (y/b)**2 <= 1
+    A = Abox * np.sum(inside) / N**2
+    return A
+
+
+
 theta_list = np.linspace(0, 2*np.pi, 101)
 x_list = a*np.cos(theta_list)
 y_list = b*np.sin(theta_list)
